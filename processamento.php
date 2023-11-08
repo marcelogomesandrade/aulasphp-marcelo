562d75e01f401d3d693bb449ed320ba9d4145484
marcelogomesandrade <gomesgomes123321@gmail.com>
2023-11-08 15:34:30 -0300

    90

---
+++ processamento.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Processamento de Dados</title>
+</head>
+
+<body>
+    <h1>Processamento de Dados vindo do formulário</h1>
+    <hr>
+    <?php
+    // Recebendo os dados via metodo POST 
+    // usando o array superglobal $_POST []
+    $nome = $_POST["nome"];
+    $email = $_POST["email"];
+    $nascimento = $_POST["nascimento"];
+    $mensagem = $_POST["mensagem"];
+
+    if (empty($nome) || empty($email)) { ?>
+
+        <p>Você deve preencher nome e e-mail!</p>
+
+    <?php
+
+    } else {
+
+    ?>
+        <h2>Dados Recebidos:</h2>
+        <ul>
+
+            <li>Nome:<?= $nome ?></li>
+            <li>E-mail:<?= $email ?></li>
+            <li>Data de Nascimento : <?= $nascimento ?></li>
+            <li>Mensagem: <?= $mensagem ?></li>
+
+        </ul>
+    <?php } ?>
+
+</body>
+
+</html>
